e83f372b1e05739956a324e376dd82fc0d4de3f5
kong-pd <240430852+kong-pd@users.noreply.github.com>
2026-09-09 13:15:39 +0800

    fix: add is_banned migration for PostgreSQL

---
+++ alembic/versions/6933c81675bf_add_is_banned_to_users.py
@@ -0,0 +1,22 @@
+﻿"""add is_banned to users
+
+Revision ID: 6933c81675bf
+Revises: 945f0e085f49
+Create Date: 2026-09-09
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+revision: str = '6933c81675bf'
+down_revision: Union[str, Sequence[str], None] = '945f0e085f49'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+def upgrade() -> None:
+    op.add_column('users', sa.Column('is_banned', sa.Integer(), nullable=False, server_default='0'))
+
+def downgrade() -> None:
+    op.drop_column('users', 'is_banned')
